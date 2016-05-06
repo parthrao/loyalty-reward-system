80130679925b7df1b861a39f5ce6241e9c200e3e
Parth <parthrao8@gmail.com>
2016-05-06 16:45:15 +0530

    implementing user authentication via OTP

--- server/utils/sendSMS.js
+++ server/utils/sendSMS.js
@@ -1,10 +1,10 @@
 //Initialize a REST client in a single line:
-var client = require('twilio')('TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN');
+var client = require('twilio')('AC520b9fd0579a3ff0f5237c2eb3399e9a', '1de3ca6fe04576ddc95dd3bb163ccfd4');
 
 // Use this convenient shorthand to send an SMS:
 client.sendSms({
-    to:'YOUR_PHONE',
-    from:'TWILIO_NUMBER',
+    to:'+919824479354',
+    from:'+12056831893',
     body:'ahoy hoy! Testing Twilio and node.js'
 }, function(error, message) {
     if (!error) {
@@ -12,7 +12,10 @@ client.sendSms({
         console.log(message.sid);
         console.log('Message sent on:');
         console.log(message.dateCreated);
+        console.log('message body is:');
+        console.log(message);
     } else {
         console.log('Oops! There was an error.');
+        console.log(error);
     }
 });
